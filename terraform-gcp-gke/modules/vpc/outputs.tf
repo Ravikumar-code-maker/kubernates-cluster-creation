9b79a37deabdb55736a22b00066b092368fb41b4
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-15 14:06:40 +0530

    Update outputs.tf

---
+++ terraform-gcp-gke/modules/vpc/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = google_compute_network.vpc.id
+}
+
+output "vpc_name" {
+  value = google_compute_network.vpc.name
+}
+
+output "subnet_name" {
+  value = google_compute_subnetwork.private_subnet.name
+}
+
+output "subnet_id" {
+  value = google_compute_subnetwork.private_subnet.id
+}
