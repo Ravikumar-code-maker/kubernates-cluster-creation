4c6a970832c8a6bae7a8638f928ad2149e4edf5a
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-15 14:02:56 +0530

    Update main.tf

---
+++ terraform-gcp-gke/modules/nat/main.tf
@@ -0,0 +1,19 @@
+resource "google_compute_router" "router" {
+  name    = var.router_name
+  network = var.network
+  region  = var.region
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               =  var.nat_name
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  enable_endpoint_independent_mapping = true
+}
