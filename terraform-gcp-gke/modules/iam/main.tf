2af1f31d4c860ea52ce42d91cc77cfe2b6741445
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-16 13:11:50 +0530

    Update main.tf

--- terraform-gcp-gke/modules/iam/main.tf
+++ terraform-gcp-gke/modules/iam/main.tf
@@ -3,6 +3,9 @@ resource "google_service_account" "gke_sa" {
   display_name = "GKE Service Account"
 }
 
+//Each node (VM) runs with that service account.
+//Then this IAM role allows the node to interact with Google services.
+
 resource "google_project_iam_member" "gke_node_role" {
   role    = "roles/container.nodeServiceAccount"
   member  = "serviceAccount:${google_service_account.gke_sa.email}"
