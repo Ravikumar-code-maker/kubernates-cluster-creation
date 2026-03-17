e15e9143571fd6bc9e70e462afd4be99dacb7793
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-17 15:17:34 +0530

    Update variables.tf

---
+++ terraform-gcp-gke/modules/gke/variables.tf
@@ -0,0 +1,23 @@
+varibale "cluster_name" {
+  type  = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "subnetwork" {
+  type = string
+}
+
+variable "network" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+}
+
+variable "service_account" {
+  type = string
+}
