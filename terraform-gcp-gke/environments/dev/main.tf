14f220c4aa04b77c2b08a57dc98488f810d84ee5
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-17 16:16:12 +0530

    Update main.tf

--- terraform-gcp-gke/environments/dev/main.tf
+++ terraform-gcp-gke/environments/dev/main.tf
@@ -8,3 +8,29 @@ module "vpc" {
   services_cidr = "10.30.0.0/16"
   region        = var.region
 }
+
+module "nat" {
+  source      = "../../mdoules/nat"
+  router_name = "dev-router"
+  nat_name    = "dev-nat"
+  network     = module.vpc.vpc_name
+  region      = var.region
+}
+
+module "iam" {
+  source               = "../../mdoules/iam"
+
+  project_id           = var.project_id
+  service_account_name = "gke-service-account"
+}
+
+module "gke" {
+  source = "../../modules/gke"
+
+  cluster_name    = "dev-gke"
+  region          = var.region
+  network         = module.vpc.vpc_name
+  subnetwork      = module.vpc.subnet_name
+  node_count      = 2
+  service_account = "gke-service-account@${var.project_id}.iam.gserviceaccount.com"
+}
